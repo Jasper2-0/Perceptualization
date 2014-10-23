d720c90ef858c5a66257818dc1f6d5be795a2469
Jasper Schelling <jasper.schelling@gmail.com>
2014-10-23 08:18:54 +0200

    Rewrote datastructure parser
    Trees always trip me up.

--- 01_Data_Wrangling/chop_gemeenten.py
+++ 01_Data_Wrangling/chop_gemeenten.py
@@ -4,23 +4,44 @@ import csv
 rawFile = 'raw/Bevolking__leeftijd,_191014225304.csv'
 testFile = 'raw/test.csv'
 
+data = []
+rows = []
+
 with open(testFile,'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=';',)
+     reader = csv.reader(csvfile, delimiter=';')
+
+
+     gemeente = ''
+     rowCount = 0;
+     for row in reader:
+         if rowCount > 3:
+             if gemeente != row[0]:
+                 data.append(rows)
+                 rows = []
+                 
+             rows.append(row)
+             gemeente = row[0]
+         rowCount = rowCount +1
+
+data.pop(0)         
+tree = {}
+
+
+for i in range(len(data)):
+    gemeente = data[i][0][0]
+    tree[gemeente] = {}
     
-    rowCount = 0;
-    
-    
-    currentCity = '';
-    currentYear = '';
-    currentGroup = '';
-    currentValue = '';
-    
-    for row in csvreader:
-        if rowCount > 3:
-            print row
-            currentCity = row[0]
-            currentYear = row[1]
-            currentGroup = row[2]
-            currentValue = row[3]
-            
-        rowCount = rowCount+1
+    for j in range(len(data[i])):
+        
+        year = data[i][j][1]
+        tree[gemeente][year] = {}
+        
+    for j in range(len(data[i])):
+        year = data[i][j][1]
+        bracket = data[i][j][2]
+        tree[gemeente][year][bracket] = data[i][j][3]
+
+
+for gemeente in tree:
+    for year in tree[gemeente]:
+        print tree[gemeente][year]
